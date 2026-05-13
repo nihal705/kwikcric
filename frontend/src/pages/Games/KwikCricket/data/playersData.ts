2c97d59dc9b9618dec8ee7a222cdfed811e28881
Nihal <nihalmohammad705@gmail.com>
2026-05-13 21:35:42 +0530

    Add KwikCricket data with Git LFS

---
+++ frontend/src/pages/Games/KwikCricket/data/playersData.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4cd477b8985941d9bcabfb4119ebadda4ef391270cf1f6e0dc8475ea4e1e532b
+size 97289
---
+++ frontend/src/pages/Games/KwikCricket/data/teamsData.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab7dbacb8ffb84a7e59f48249af355281ab9eb9f1950f0225c09b5dc330bf58e
+size 3278
